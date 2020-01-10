9904656d1969d8d98acccb95f8b3e3634f86747e
Mercedes Walsh <sadieswalsh@gmail.com>
2020-01-09 20:58:58 -0800

    added react files and functionality

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = _ => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">To Do List</span>
+        </nav>
+    )
+}
+
+export default Nav
