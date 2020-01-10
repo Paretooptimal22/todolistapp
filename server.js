8b466721e8e8ab681f77048cbcc0ad81053d5991
chbbs231 <chbbs231@yahoo.com>
2020-01-09 20:03:33 -0800

    working on connection for mongoose

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+require ('./config')
 const express = require('express')
 const { join } = require('path')
-
 const app = express()
 
 app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+require('./routes')(app)
+
 app.listen(process.env.PORT || 3001)
+
+require('mongoose').connection.once('open', () => app.listen(3000)) 
+
