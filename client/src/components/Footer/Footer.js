4331860b0e0a25dc029afe310705391ca848059f
Mercedes Walsh <sadieswalsh@gmail.com>
2020-01-11 12:32:20 -0800

    changes components to hooks

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,11 +2,6 @@ import React from 'react'
 
 const Footer = _ => {
     return (
-        // <footer className="footer">
-        //     <div className="container fixed-bottom">
-        //         <span className="text-muted">Copyright 2020 ToDo</span>
-        //     </div>
-        // </footer>
 
         <nav class="navbar fixed-bottom navbar-dark bg-dark">
             <p className="text-muted">Copyright 2020 ToDo</p>
--- client/src/components/ItemList/ItemList.js
+++ client/src/components/ItemList/ItemList.js
@@ -1,6 +1,6 @@
 // items for to-do list
 
-import React from 'react'
+import React, { useContext } from 'react'
 import FormContext from '../../utils/FormContext'
 
 // const ItemList = props => {
@@ -16,22 +16,43 @@ import FormContext from '../../utils/FormContext'
 // }
 
 const ItemList = _ => {
+
+    const { items } = useContext(FormContext)
+
     return (
-        <FormContext.Consumer>
-            {
-                ({ item, items }) => (
-                    <>
-                    <h4>Things to do:</h4>
-                        <ul className="list-group">
-                            {
-                                items.map((item, i) => <li className="list-group-item" key={i}>{item}</li>)
-                            }
-                        </ul>
-                    </>
-                )
-            }
-        </FormContext.Consumer>
+        <>
+        <h4>Things to do:</h4>
+            {/* <ul className="list-group">
+                {
+                    items.map((item, i) => <li className="list-group-item" key={i}>{item}</li>)
+                }
+            </ul> */}
+            <ul className="list-group">
+                {
+                    items.length ? items.map(item => <li className="list-group-item">{item}</li>) : null
+                }
+            </ul>
+        </>
     )
+
 }
 
 export default ItemList
+
+// without hooks
+    // return (
+    //     <FormContext.Consumer>
+    //         {
+    //             ({ item, items }) => (
+    //                 <>
+    //                 <h4>Things to do:</h4>
+    //                     <ul className="list-group">
+    //                         {
+    //                             items.map((item, i) => <li className="list-group-item" key={i}>{item}</li>)
+    //                         }
+    //                     </ul>
+    //                 </>
+    //             )
+    //         }
+    //     </FormContext.Consumer>
+    // )
